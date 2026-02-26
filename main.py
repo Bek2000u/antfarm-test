638671eb6b967d1ae1273caa1400831c0b4cffa0
root <root@vmi3104035.contaboserver.net>
2026-02-26 23:52:09 +0100

    Add requirements.txt and Makefile

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root() -> dict[str, str]:
+    return {"message": "hello world"}
+
+
+@app.get("/health")
+def health() -> dict[str, str]:
+    return {"status": "ok"}
